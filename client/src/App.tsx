3200f42989cd521de2a638e4c1e706ff635ec545
m3rashid <m3rashid.hussain@gmail.com>
2024-01-14 10:04:53 +0530

    works, but can we do better?

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,27 @@
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 
-import React, { useEffect } from 'react';
+import Tus from '@uppy/tus';
 import Uppy from '@uppy/core';
+import { onMount } from 'solid-js';
 import Dashboard from '@uppy/dashboard';
 
-import Tus from '@uppy/tus';
-
 function App() {
-  const appRef = React.useRef(null);
-  const [uppy, setUppy] = React.useState<Uppy>();
-
-  useEffect(() => {
-    if (uppy) return;
-    if (appRef.current && !uppy) {
-      const newUppy = new Uppy({ debug: true })
-        .use(Dashboard, { inline: true, target: '#uppy-dashboard' })
-        .use(Tus, { endpoint: 'http://localhost:5000/files/' });
-      setUppy(newUppy);
-    }
-  }, []);
+  onMount(() => {
+    new Uppy({ debug: true })
+      .use(Dashboard, { inline: true, target: '#uppy-dashboard' })
+      .use(Tus, { endpoint: 'http://localhost:5000/files/' });
+  });
 
   return (
     <>
-      <img
-        height={500}
-        src='http://localhost:5000/file/d346bbee91bc6dd98d93e2563113275c'
-        // src='https://s3.ap-south-1.amazonaws.com/go.awesome.stack/d346bbee91bc6dd98d93e2563113275c'
-      />
-      {/* <video
-        controls
-        height={500}
-        src='http://localhost:5000/file/40b011750f6893fb772ff68844997e2b'
-      /> */}
-      <div id='uppy-dashboard' ref={appRef} />
+      {[
+        'd346bbee91bc6dd98d93e2563113275c',
+        '199764f14e0b6fbedb53452c6d37eb06',
+      ].map((imageId) => (
+        <img height={500} src={`http://localhost:5000/file/${imageId}`} />
+      ))}
+      <div id='uppy-dashboard' />
     </>
   );
 }
